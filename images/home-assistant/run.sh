4af3f7954646d29945a91346aeac18f502b15bde
Patrick Bozeman <pbozeman@gmail.com>
2023-11-26 03:31:33 -0800

    fix ha run script

--- images/home-assistant/run.sh
+++ images/home-assistant/run.sh
@@ -2,8 +2,12 @@
 #
 # This is for running the image during development
 
+REGISTRY_URL="registry.blinkies.io"
+REPOSITORY_NAME="home-assistant"
+IMAGE_TAG="stable"
+
 docker run \
 	--restart=unless-stopped \
 	-v ./.config:/config \
 	--network=host \
-	blinkies/home-assistant:stable
+	$REPOSITORY_NAME:$IMAGE_TAG
