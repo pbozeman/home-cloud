012bb355de8279a7e71ae86d51feab21270d2a16
Patrick Bozeman <pbozeman@gmail.com>
2023-11-28 17:02:37 -0800

    make taint-k3s work from any dir

--- terraform/scripts/taint-k3s.sh
+++ terraform/scripts/taint-k3s.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Navigate to the directory of this script
+cd $(dirname $(readlink -f $0))
+cd ..
+
 # Find all state resources matching the pattern
 resource_pattern="module.vms.proxmox_virtual_environment_vm.nixos_vms\[\"k3s-.*\"\]"
 matching_states=($(terraform state list | grep -E "$resource_pattern"))
