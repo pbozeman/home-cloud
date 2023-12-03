ba6b77f2003e0bc7b78c9b9ed0e8c56347227117
Patrick Bozeman <pbozeman@gmail.com>
2023-12-02 19:37:25 -0800

    appdaemon app to create group lights_all

---
+++ helmfile/charts/appdaemon/config-directory/apps/light-group.py
@@ -0,0 +1,13 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+
+class LightGroupAll(hass.Hass):
+    def initialize(self):
+        all_lights = self.get_state("light")
+        light_entities = [entity for entity in all_lights.keys()]
+        self.create_light_group("lights_all", "All Lights", light_entities)
+
+    def create_light_group(self, group_name, friendly_name, entities):
+        self.call_service(
+            "group/set", object_id=group_name, name=friendly_name, entities=entities
+        )
