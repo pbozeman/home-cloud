12e6d42b186de9c601dc159368626b7f6a1ca830
Patrick Bozeman <pbozeman@gmail.com>
2023-12-04 14:29:22 -0800

    motion light enhancements
    * log_state
    * blocked support
    * revert need_illumination to correctly use the sun

--- helmfile/charts/appdaemon/config-directory/apps/motion-light.py
+++ helmfile/charts/appdaemon/config-directory/apps/motion-light.py
@@ -18,13 +18,16 @@ class MotionLight(hass.Hass):
         # lights it turns on are already on.
         #
         # The night_mode_entity works the same way.
+        # Night entity defaults to None since the norm is to not turn on any
+        # lights.
         self.day_mode_entity = self.args.get("day_mode_entity", self.entity)
-        self.night_mode_entity = self.args.get("night_mode_entity", self.entity)
+        self.night_mode_entity = self.args.get("night_mode_entity", None)
         self.night_mode_boolean = self.args.get(
             "night_mode_boolean", "input_boolean.goodnight_mode"
         )
 
         # misc settings
+        self.blocker = self.args.get("blocker", None)
         self.motion_off_delay_sec = self.args.get("delay_sec", 5)
         self.turn_on_enabled = self.args.get("turn_on", True)
         self.turn_off_enabled = self.args.get("turn_off", True)
@@ -51,14 +54,10 @@ class MotionLight(hass.Hass):
         self.listen_state(self.entity_on, self.entity, new="on")
 
     def motion_recent(self, entity, attribute, old, new, kwargs):
-        self.log(
-            "motion_recent"
-            + f" state: '{self.get_state(self.entity)}'"
-            + f" need_illumination: '{self.need_illumination()}'"
-            + f" turn_on_enabled: '{self.turn_on_enabled}'"
-            + f" is_night_mode: '{self.is_night_mode()}'"
-            + f" entity_to_turn_on: '{self.entity_to_turn_on()}'"
-        )
+        self.log_state("motion_recent")
+
+        if self.is_blocked():
+            return
 
         # we do this before the illumination and turn_on_enabled checks,
         # because regardless of their policy, we want to (potentially)
@@ -79,12 +78,22 @@ class MotionLight(hass.Hass):
             self.turn_on(entity)
 
     def entity_on(self, entity, attribute, old, new, kwargs):
+        self.log_state("entity_on")
+
+        if self.is_blocked():
+            return
+
         # kick off the timer when manually turned on too
-        self.log("entity_on")
         self.set_off_timer()
 
+    def is_blocked(self):
+        if not self.blocker:
+            return False
+
+        return self.get_state(self.blocker) == "on"
+
     def need_illumination(self):
-        return self.sun_up()
+        return self.sun_down()
 
     def is_night_mode(self):
         return self.get_state(self.night_mode_boolean) == "on"
@@ -117,3 +126,14 @@ class MotionLight(hass.Hass):
 
     def log(self, str):
         super().log(f"MotionLight {self.entity} {str}")
+
+    def log_state(self, str):
+        return self.log(
+            str
+            + f" state: '{self.get_state(self.entity)}'"
+            + f" is_blocked: '{self.is_blocked()}'"
+            + f" need_illumination: '{self.need_illumination()}'"
+            + f" turn_on_enabled: '{self.turn_on_enabled}'"
+            + f" is_night_mode: '{self.is_night_mode()}'"
+            + f" entity_to_turn_on: '{self.entity_to_turn_on()}'"
+        )
