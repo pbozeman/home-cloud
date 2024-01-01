00ea4c5281bc80f6ce04954506f025221d6bbbd0
Patrick Bozeman <pbozeman@gmail.com>
2023-12-31 16:15:37 -0800

    [tandoor] added helm chart and backup

--- helmfile/scripts/create-daily-schedule.sh
+++ helmfile/scripts/create-daily-schedule.sh
@@ -2,7 +2,10 @@
 
 SCHEDULE_NAME="daily-home"
 SCHEDULE_TIME="0 3 * * *"
-NAMESPACES="home-automation,media,paperless-ngx,immich"
+
+# TODO: move to using annotations rather than having to manage
+# lists in the script
+NAMESPACES="home-automation,media,paperless-ngx,immich,tandoor"
 
 # Check if the schedule already exists
 velero schedule get $SCHEDULE_NAME >/dev/null 2>&1
