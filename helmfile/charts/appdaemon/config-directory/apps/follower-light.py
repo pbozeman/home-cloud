25bca67d1c5fe2b5bfe836e8cfbfc8fd9c5495a3
Patrick Bozeman <pbozeman@gmail.com>
2024-01-01 19:48:24 -0800

    [appdaemon] common lights follow dining room

--- helmfile/charts/appdaemon/config-directory/apps/follower-light.py
+++ helmfile/charts/appdaemon/config-directory/apps/follower-light.py
@@ -14,16 +14,21 @@ class FollowerLight(hass.Hass):
             self.log("no follower specified")
             return
 
+        self.ignore_on = self.args.get("ignore_on", False)
+        self.ignore_off = self.args.get("ignore_off", False)
+
         self.listen_state(self.leader_turned_on, self.leader, new="on")
         self.listen_state(self.leader_turned_off, self.leader, new="off")
 
     def leader_turned_on(self, entity, attribute, old, new, kwargs):
-        self.log("leader_turned_on")
-        self.turn_on(self.follower)
+        self.log(f"leader_turned_on ignore_on: {self.ignore_on}")
+        if not self.ignore_on:
+            self.turn_on(self.follower)
 
     def leader_turned_off(self, entity, attribute, old, new, kwargs):
-        self.log("leader_turned_off")
-        self.turn_off(self.follower)
+        self.log(f"leader_turned_off ignore_off: {self.ignore_off}")
+        if not self.ignore_off:
+            self.turn_off(self.follower)
 
     def log(self, str):
-        super().log(f"FollowerLight {str}")
+        super().log(f"FollowerLight {self.leader} -> {self.follower} {str}")
