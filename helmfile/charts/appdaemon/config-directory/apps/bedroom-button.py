477afd6ac21fd9eaa05bd053b4e6c53103309618
Patrick Bozeman <pbozeman@gmail.com>
2024-04-08 16:29:37 -0700

    [appdaemon] add bedroom button

---
+++ helmfile/charts/appdaemon/config-directory/apps/bedroom-button.py
@@ -0,0 +1,43 @@
+import appdaemon.plugins.hass.hassapi as hass  # type: ignore
+
+import appdaemon.plugins.hass.hassapi as hass
+
+
+class BedroomButton(hass.Hass):
+    def initialize(self):
+        self.log("init")
+        self.listen_event(self.button_event, "lutron_caseta_button_event")
+
+    def button_event(self, event_name, data, kwargs):
+        self.log(f"Lutron button event received: {data}")
+
+        if data.get("area_name") != "Main Bedroom":
+            return
+
+        if data.get("device_name") != "Pico":
+            return
+
+        if data.get("action") != "press":
+            return
+
+        button = data.get("button_number")
+        if button == 0:
+            self.on()
+        elif button == 2:
+            self.off()
+
+    def on(self):
+        self.log("on")
+        self.turn_on(self.goodnight)
+
+    def off(self):
+        self.log("off")
+
+    def up(self):
+        self.log("up")
+
+    def down(self):
+        self.log("down")
+
+    def log(self, str):
+        super().log(f"BedroomButton {str}")
