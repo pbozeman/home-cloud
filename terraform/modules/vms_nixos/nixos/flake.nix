ca43607c67f030445f3dc2e796bad151e293e817
Patrick Bozeman <pbozeman@gmail.com>
2024-09-04 23:01:09 -0700

    [nixos] fix comment in templated base flake

--- terraform/modules/vms_nixos/nixos/flake.nix
+++ terraform/modules/vms_nixos/nixos/flake.nix
@@ -1,2 +1,7 @@
 # nix needs the file checked in, but this will get overwritten from template
-# expansion. It is also in .gitignore
+# expansion. To make git ignore changes:
+#
+#  git update-index --assume-unchanged modules/vms_nixos/nixos/flake.nix
+#
+# TODO: add the above command to some sort of local git init script that
+# inits things that aren't part of the actual sync'd repo.
